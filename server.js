7a35443f67fe76c5d483280af64ee0463536218f
yamamot000 <167596865+yamamot000@users.noreply.github.com>
2024-10-16 19:28:29 +0800

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const http = require('http');
-const socketIo = require('socket.io');
+const socketIo = require('socket.io');  // Import socket.io
 const app = express();
 const port = 3000;
 
@@ -12,17 +12,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const server = http.createServer(app);
 const io = socketIo(server);
-
 app.get('/join-queue', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/queue.html'));
 });
+
 io.on('connection', (socket) => {
     console.log('A client connected');
     socket.on('customer-scanned', (data) => {
         console.log('Customer scanned:', data);
         io.emit('update-queue', data);
     });
-
     socket.on('disconnect', () => {
         console.log('A client disconnected');
     });
