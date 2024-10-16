1544dd50644dd94d275f946982dba4616cac58c2
yamamot000 <167596865+yamamot000@users.noreply.github.com>
2024-10-16 20:10:36 +0800

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const http = require('http');
-const socketIo = require('socket.io');  // Import socket.io
+const socketIo = require('socket.io');
 const app = express();
 const port = 3000;
 
